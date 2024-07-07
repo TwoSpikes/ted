4ad35e568a31bb09351e59e84b1e921f7c68dd1b
Two Spikes <twospikesfpg@gmail.com>
2024-07-07 12:56:39 +0300

    feat: Read input characters

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use cursive::CursiveExt;
+use std::io::Write;
 
 fn main() {
-    let mut app = ::cursive::Cursive::default();
-    app.add_layer(::cursive::views::Dialog::text("Hello, world!"));
-    app.run();
+    let term = ::console::Term::stdout();
+    let mut stdout = ::std::io::stdout();
+    loop {
+        let ch = term.read_char().expect("Cannot read char");
+        if ch == 'q' {
+            break;
+        }
+        print!("{}", ch);
+        _ = stdout.flush();
+    }
 }
