09e6b450dd58de55164b722c2cb2abe3cfef6352
Two Spikes <twospikesfpg@gmail.com>
2024-07-07 17:09:55 +0300

    feat: Implement Normal and Insert mode

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,31 @@ use ::std::io::Write;
 use ::termion::cursor::DetectCursorPos;
 use termion::raw::IntoRawMode;
 
+enum Mode {
+    NORMAL,
+    INSERT,
+}
+impl ToString for Mode {
+    fn to_string(&self) -> String {
+        String::from(match self {
+            Mode::NORMAL => "NORMAL",
+            Mode::INSERT => "INSERT",
+        })
+    }
+}
+
 fn main() {
+    let mut mode = Mode::NORMAL;
+
     let mut stdout = ::std::io::stdout().into_raw_mode().expect("Cannot get stdout in raw mode");
     _ = ::crossterm::terminal::enable_raw_mode();
     let ::termsize::Size{rows, cols: _} = ::termsize::get().expect("Cannot get terminal size");
     print!("{}",
         ::termion::clear::All
     );
-    print!("{}NORMAL{}",
+    print!("{}{}{}",
         ::termion::cursor::Goto(0, rows),
+        mode.to_string(),
         ::termion::cursor::Goto(0, 1),
     );
     _ = stdout.flush();
@@ -24,20 +40,56 @@ fn main() {
             if code == ::crossterm::event::KeyCode::Char('q') {
                 break;
             }
-            match code {
-                ::crossterm::event::KeyCode::Backspace => {
-                    let (x, y) = stdout.cursor_pos().expect("Cannot get cursor position");
-                    print!("{} {}",
-                        ::termion::cursor::Goto(x-1, y),
-                        ::termion::cursor::Goto(x-1, y),
-                    );
-                    _ = stdout.flush();
+            match mode {
+                Mode::NORMAL => {
+                    match code {
+                        ::crossterm::event::KeyCode::Char('i') => {
+                            let (x, y) = stdout.cursor_pos().expect("Cannot get cursor position");
+                            mode = Mode::INSERT;
+                            print!("{}{}{}",
+                                ::termion::cursor::Goto(0, rows),
+                                mode.to_string(),
+                                ::termion::cursor::Goto(x, y),
+                            );
+                        },
+                        _ => {
+
+                        },
+                    }
                 },
-                ::crossterm::event::KeyCode::Char(c) => {
-                    print!("{}", c);
-                    _ = stdout.flush();
+                Mode::INSERT => {
+                    match code {
+                        ::crossterm::event::KeyCode::Esc => {
+                            let (x, y) = stdout.cursor_pos().expect("Cannot get cursor position");
+                            mode = Mode::NORMAL;
+                            print!("{}{}{}",
+                                ::termion::cursor::Goto(0, rows),
+                                mode.to_string(),
+                                ::termion::cursor::Goto(x, y),
+                            );
+                            _ = stdout.flush();
+                        },
+                        ::crossterm::event::KeyCode::Backspace => {
+                            let (x, y) = stdout.cursor_pos().expect("Cannot get cursor position");
+                            print!("{} {}",
+                                ::termion::cursor::Goto(x-1, y),
+                                ::termion::cursor::Goto(x-1, y),
+                            );
+                            _ = stdout.flush();
+                        },
+                        ::crossterm::event::KeyCode::Enter => {
+                            let (_, y) = stdout.cursor_pos().expect("Cannot get cursor position");
+                            println!("{}",
+                                ::termion::cursor::Goto(0, y)
+                            );
+                        },
+                        ::crossterm::event::KeyCode::Char(c) => {
+                            print!("{}", c);
+                            _ = stdout.flush();
+                        },
+                        _ => todo!(),
+                    }
                 },
-                _ => todo!(),
             }
         }
     }
